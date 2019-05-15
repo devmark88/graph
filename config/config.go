8c555eb710da20cccb3224501c90d8e0796f63fd
Mark Karamyar <mark.karamyar@gmail.com>
2019-05-15 14:27:04 +0430

    fix postgres port number

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ type Specs struct {
 	Port             int    `split_words:"true" default:"9999"`
 	DatabaseHost     string `split_words:"true" default:"127.0.0.1"`
 	DatabaseUsername string `split_words:"true" default:"mark" `
-	DatabasePort     string `split_words:"true" default:"5432"`
+	DatabasePort     int    `split_words:"true" default:"5432"`
 	DatabasePassword string `split_words:"true" default:"123456"`
 	DatabaseName     string `split_words:"true" default:"unireg"`
 }
